35e4841a546db0ed8317b7e4a929562ae75a18c8
Bjorn Ironside <90470039+sumanpokhrel-11@users.noreply.github.com>
2024-04-08 07:31:49 +0545

    updated using list comprehension

--- Day 5/passwordgenerator.py
+++ Day 5/passwordgenerator.py
@@ -7,21 +7,22 @@ character_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M
                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 number_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbol_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(']
-password_list = []
 charnum = int(input("enter the number of characters you want:"))
 numbernum = int(input("How many numbers do you want:"))
 symbolnum = int(input("enter the number symbol of  you want:"))
 
-for n in range(1, charnum+1):
-    password_list.append(random.choice(character_list))
-for n in range(1, numbernum+1):
-    password_list.append(random.choice(number_list))
-for n in range(1, symbolnum+1):
-    password_list.append(random.choice(symbol_list))
+# list comprehension
+password_char = [random.choice(character_list) for _ in range(1, charnum+1)]
+password_num = [random.choice(number_list) for _ in range(1, numbernum+1)]
+password_sym = [random.choice(password_list) for _ in range(1, symbolnum+1)]
 
-print(password_list)
+password_list = password_char + password_num + password_sym
+
+
+
+# print(password_list)
 random.shuffle(password_list)
-print(password_list)
+# print(password_list)
 # harder one
 password = " "
 for char in password_list:
