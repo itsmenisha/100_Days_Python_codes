be2e2cf4ca06a2bf75dc3cbd839081c027521f8c
itsmenisha <90882851+itsmenisha@users.noreply.github.com>
2024-05-07 16:41:06 +0545

    eventlitioners.py

--- Day19/eventlitioners.py
+++ Day19/eventlitioners.py
@@ -1,11 +1,6 @@
 from turtle import Turtle, Screen
 import random
 import turtle as t
-t.colormode(255)
-nisha = Turtle()
-nisha.speed("fastest")
-screen = Screen()
-
 
 def move_forward():
     nisha.forward(10)
