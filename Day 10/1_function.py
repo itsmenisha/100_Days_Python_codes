b0de23cf3d516a78803a1933c73cc8717aaf5c53
itsmenisha <90882851+itsmenisha@users.noreply.github.com>
2025-09-07 05:39:36 +0545

    1_function.py

--- Day 10/1_function.py
+++ Day 10/1_function.py
@@ -3,8 +3,8 @@ def format_name(f_name, l_name):
         return "You didnt provide valid inputs"
     return ((f_name+l_name).title())
 
-
 f_name = (input("enter your first name : ")).lower()
 l_name = (input("enter your last name : ")).lower()
 
 print(format_name(f_name, l_name))
+
